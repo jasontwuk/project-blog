d6ba29c085df6b40413fe04bcd659683ec974361
Jason <jasontwuk@gmail.com>
2024-06-21 10:59:06 +0100

    soluation for exercise 5B: add <MotionConfig>

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,8 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
 
+import RespectMotionPreferences from '@/components/RespectMotionPreferences';
+
 export const metadata = {
   title: BLOG_TITLE,
   description: 'A wonderful blog about JavaScript',
@@ -34,18 +36,20 @@ function RootLayout({ children }) {
   const theme = 'light';
 
   return (
-    <html
-      lang="en"
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RespectMotionPreferences>
+      <html
+        lang="en"
+        className={clsx(mainFont.variable, monoFont.variable)}
+        data-color-theme={theme}
+        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+      >
+        <body>
+          <Header theme={theme} />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RespectMotionPreferences>
   );
 }
 
